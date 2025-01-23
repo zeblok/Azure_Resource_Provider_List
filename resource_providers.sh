84492136e46cfa3b881e028c4c56d59738f7601d
kabali126 <146117808+kabali126@users.noreply.github.com>
2025-01-23 11:52:12 +0530

    Create resource_providers.sh

---
+++ resource_providers.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+providers=(
+    "microsoft.support"
+    "Microsoft.SerialConsole"
+    "Microsoft.Resources"
+    "Microsoft.ResourceNotifications"
+    "Microsoft.ResourceGraph"
+    "Microsoft.Portal"
+    "Microsoft.MarketplaceOrdering"
+    "Microsoft.Features"
+    "Microsoft.CostManagement"
+    "Microsoft.Consumption"
+    "Microsoft.Commerce"
+    "Microsoft.ClassicSubscription"
+    "Microsoft.Billing"
+    "Microsoft.Authorization"
+    "Microsoft.ADHybridHealthService"
+    "Microsoft.Network"
+    "Microsoft.Compute"
+    "Microsoft.OperationalInsights"
+    "Microsoft.ManagedIdentity"
+    "Microsoft.Blueprint"
+    "Microsoft.Storage"
+    "Microsoft.DataProtection"
+    "Microsoft.DevTestLab"
+    "Microsoft.ContainerService"
+    "Microsoft.Advisor"
+)
+
+for provider in "${providers[@]}"
+do
+    echo "Registering $provider..."
+    az provider register --namespace $provider
+    if [ $? -eq 0 ]; then
+        echo "$provider registered successfully."
+    else
+        echo "Failed to register $provider."
+    fi
+done
